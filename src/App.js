a4be475dc9dbfe562658c8bf197c2e73891fafc0
Asutosh Acharya <asutosh246@gmail.com>
2023-10-15 20:02:16 +0530

    some basics

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="border-4 border-green-500 bg-random h-50% w-screen flex justify-center gap-12">
+      <p className="text-green-600 text-sm">Tailwind css</p>
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        random: "#7f32a8",
+      },
+    },
   },
   plugins: [],
 };
