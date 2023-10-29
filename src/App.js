e8228a1a61ddefb85d0734db02805aaecb04cb32
Asutosh Acharya <asutosh246@gmail.com>
2023-10-29 18:25:05 +0530

    common used tailwind classes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 function App() {
   return (
     <div className="border-4 border-green-500 bg-random h-50% w-screen flex justify-center gap-12">
-      <p className="text-green-600 text-sm">Tailwind css</p>
+      <p className="text-green-600 text-sm underline decoration-wavy decoration-red-400 underline-offset-2 capitalize break-all">
+        Tailwind css Starting to learn adasdaaaaaa aaaaaaaaaaaaaaaaaaaaaaaa
+      </p>
     </div>
   );
 }
